41e27d2c39313ef6591f605d852bb2e0033a857a
Francisco-BT <pancho10810@gmail.com>
2023-08-22 16:42:23 -0600

    feat: add product and category table

---
+++ src/db/models/category.model.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+
+const CATEGORY_TABLE = 'category';
+
+const CategorySchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at',
+    defaultValue: DataTypes.NOW,
+  },
+};
+
+class Category extends Model {
+  static associate(models) {
+    this.hasMany(models.Product, {
+      as: 'products',
+      foreignKey: 'categoryId',
+    });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CATEGORY_TABLE,
+      modelName: 'Category',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { CATEGORY_TABLE, CategorySchema, Category };
--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -2,18 +2,21 @@ const Joi = require('joi');
 
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(50);
+const description = Joi.string().min(10);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  description: description.required(),
   image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  description,
   image,
 });
 
